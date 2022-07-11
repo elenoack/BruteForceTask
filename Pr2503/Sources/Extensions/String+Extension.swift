d8ead1956c51564ad53fe11610d8ec17da850ae9
elenoack <elenanoack@mail.ru>
2022-07-11 12:47:16 +0300

    add_extensions

---
+++ Pr2503/Sources/Extensions/String+Extension.swift
@@ -0,0 +1,29 @@
+//
+//  String+Extension.swift
+//  Pr2503
+//
+//  Created by Elena Noack on 11.07.22.
+//
+
+import Foundation
+
+extension String {
+    var digits:      String { return "0123456789" }
+    var lowercase:   String { return "abcdefghijklmnopqrstuvwxyz" }
+    var uppercase:   String { return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }
+    var punctuation: String { return "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" }
+    var letters:     String { return lowercase + uppercase }
+    var printable:   String { return digits + letters + punctuation }
+
+    mutating func replace(at index: Int, with character: Character) {
+        var stringArray = Array(self)
+        stringArray[index] = character
+        self = String(stringArray)
+    }
+}
+
+extension Optional where Wrapped == String {
+    var isEmptyOrNil: Bool {
+        return self?.isEmpty != false
+    }
+}
